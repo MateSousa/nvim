2c0dc9b48d78ffde50897d74d1bebfae7aa38612
MateSousa <matessousa@outlook.com>
2023-11-15 20:38:52 -0300

    ✨ feat(keymaps.lua): add new keymaps for improved navigation and functionality New keymaps have been added to the keymaps.lua file to enhance the user experience. These keymaps include commands for closing the nvim tab, finding files with telescope, selecting all and copying, finding a word in the current buffer or project, navigating between buffers, and changing to NeoTree. These changes aim to improve navigation and functionality within the application, making it more user-friendly and efficient to use.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymaps = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Close nvim tab with `,q`
+keymaps("n", "<leader>q", ":q<cr>", opts)
+
+-- Find files with telescope using <leader>ff
+keymaps("n", "<leader>ff", ":Telescope find_files<cr>", opts)
+
+-- Select all and copy with <leader>yy
+keymaps("n", "<leader>yy", ":%y+<cr>", opts)
+
+-- Find a word in the current buffer
+keymaps("n", "<leader>fw", "*N", opts)
+
+-- Find a word in the current project
+keymaps("n", "<leader>ff", ":Telelescope current_buffer_fuzzy_find<cr>", opts)
+
+-- Navigate to the previous buffer
+keymaps("n", "<leader>bp", ":bprevious<cr>", opts)
+
+-- Navigate to the next buffer
+keymaps("n", "<leader>bn", ":bnext<cr>", opts)
+
+-- Change to NeoTree
+keymaps("n", "<leader>nt", ":Neotree<cr>", opts)
